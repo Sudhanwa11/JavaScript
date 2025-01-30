1adbe5c5ef059b1e775889ce9155df7b2b6fd42a
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2025-01-30 16:08:11 +0530

    Create nested arrays.js

---
+++ nested arrays.js
@@ -0,0 +1,22 @@
+var myplants = [
+    {
+        "type" : "flowers",
+        "list" : [
+            "rose",
+            "tulip",
+            "dandelion"
+        ],
+    },
+    {
+        "type" : "trees",
+        "list" : [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+];
+
+var sectree = myplants[1].list[1];
+
+console.log(sectree);
